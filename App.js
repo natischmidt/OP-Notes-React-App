6dee3117d95818a93eef78be2f3b3ac60e42b2f1
nati schmidt <nati.schmidt97@outlook.de>
2023-05-15 18:04:42 +0200

    home screen logo WIP

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import Home from "./screens/Home";
 import {NavigationContainer} from "@react-navigation/native";
-import {createNativeStackNavigator} from "react-native-screens/native-stack";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import Notes from "./screens/Notes";
 
 
--- screens/Home.js
+++ screens/Home.js
@@ -1,20 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import { StatusBar } from "expo-status-bar";
 // import {View} from "react-native";
 // import Header from "./Header"
-import {NavigationContainer} from "@react-navigation/native";
-import 'react-native-gesture-handler';
-
+// import {NavigationContainer} from "@react-navigation/native";
 
 export default function Home() {
 
-    return (
 
-        <View>
-            <Text> test </Text>
+    return (
+        <View style={styles.container}>
+            <StatusBar style='auto'/>
+            <Image
+                style={styles.logo}
+                source={require("../assets/notesIcon.jpg")}
+            />
         </View>
 
-        // <View>
-        //     <Header/>
-        // </View>
     );
 }
+const styles = {
+    container: {
+     backgroundColor: `#4682b4`,
+    },
+
+    logo: {
+        marginLeft:60,
+        marginRight:50,
+        marginTop: 50,
+        width: 250,
+        height: 250,
+        marginBottom: 50
+    }
+
+};
